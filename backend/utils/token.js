09422ecaa94508d8edcf354bc5fae344cedb842b
Mayank14khurana <153353155+Mayank14khurana@users.noreply.github.com>
2025-07-03 01:43:50 +0530

    Update token.js

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -3,7 +3,8 @@ const generateToken =(res,user ,message)=>{
        const token =jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
        return res.status(200).cookie('token',token,{
                httpOnly:true,
-               sameSite:'strict',
+                secure: true, 
+                sameSite: 'None',
                maxAge:24*60*60*1000
        }).json({
         success:true,
